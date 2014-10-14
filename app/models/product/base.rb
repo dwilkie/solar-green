7a64dca9725798d43e0082d6bafea3b2b8381ee7
samda <muy.samda13@gmail.com>
2014-10-14 17:50:26 +0700

    fix constantize

--- app/models/product/base.rb
+++ app/models/product/base.rb
@@ -1,6 +1,6 @@
 class Product::Base < ActiveRecord::Base
   self.table_name = :products
-  TYPES = [Product::Solar, Product::Battery, Product::ChargeController, Product::GridTie]
+  TYPES = ["Product::Solar", "Product::Battery", "Product::ChargeController", "Product::GridTie"]
 
   validates_lengths_from_database
   validates :name, presence: true
